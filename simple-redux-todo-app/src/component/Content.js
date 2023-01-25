e37103928e1fb3656cebaff960783765b48a50ed
Burak Cihan <burakcihan622@gmail.com>
2023-01-25 16:49:44 +0300

    test webstorm

--- simple-redux-todo-app/src/component/Content.js
+++ simple-redux-todo-app/src/component/Content.js
@@ -2,16 +2,15 @@ import React from "react";
 import ContentFooter from "./ContentFooter";
 import TodoList from "./TodoList";
 
-const hendleChanges = ()=>{
-let burak
-}
+const Burak = () => {
+  let burak;
+};
 const Content = () => {
   return (
     <>
       <section className="main">
         <input className="toggle-all" type="checkbox" />
         <label htmlFor="toggle-all">Mark all as complete</label>
-
         <TodoList />
       </section>
       <ContentFooter />
